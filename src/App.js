bef3f4161094bff497dda66d6fcf698f50cab131
gladsonsaji <gladsonsaji4@gmail.com>
2021-06-03 15:43:58 +0530

    login and redirect to home

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Signup from "./Pages/Signup";
  * ?  =====Import Components=====
  */
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
         </Route>
         <Route path="/signup">
           <Signup />
+        </Route>
+        <Route path="/login">
+          <Login />
         </Route>
       </Router>
     </div>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
 
-import Logo from '../../olx-logo.png';
-import './Login.css';
-
+import Logo from "../../olx-logo.png";
+import "./Login.css";
+import { FirebaseContext } from "../../store/FirebaseContext";
+import { useHistory } from "react-router-dom";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { firebase } = useContext(FirebaseContext);
+  const history = useHistory();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        history.push("/");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
   return (
     <div>
       <div className="loginParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit={handleLogin}>
           <label htmlFor="fname">Email</label>
           <br />
           <input
             className="input"
             type="email"
+            value={email}
             id="fname"
+            onChange={(e) => setEmail(e.target.value)}
             name="email"
             defaultValue="John"
           />
@@ -25,6 +44,8 @@ function Login() {
             className="input"
             type="password"
             id="lname"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             name="password"
             defaultValue="Doe"
           />
